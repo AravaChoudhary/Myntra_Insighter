082a0a9fabee599e2aa9b59561e28fb8a78c8619
Arava Choudhary <aravachoudhary11@gmail.com>
2024-10-19 12:54:46 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'MyntraScrapper',
+    version = '0.0.1',
+    author = 'Nainika',
+    author_email = 'aravachoudhary11@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
